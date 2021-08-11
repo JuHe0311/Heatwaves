8e6947ecc10c185917e2ca643d5510a8363ca44a
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-08-11 14:25:35 +0200

    Update DeepGraph.py

--- create_deepGraph/DeepGraph.py
+++ create_deepGraph/DeepGraph.py
@@ -93,7 +93,6 @@ cpv['g_ids'] = gv['g_id'].apply(set)
 print(cpv)
 # append cardinality of g_id sets
 cpv['n_unique_g_ids'] = cpv['g_ids'].apply(len)
-print(cpv['time_amax'].loc[1].type())
 # append time spans
 cpv['dt'] = cpv['time_amax'] - cpv['time_amin']
 
