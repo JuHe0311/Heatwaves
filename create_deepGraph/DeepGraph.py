f4950cd4ad485f82e8e6f540fb3411e8a4daffc7
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-08-12 13:35:44 +0200

    Update DeepGraph.py

--- create_deepGraph/DeepGraph.py
+++ create_deepGraph/DeepGraph.py
@@ -12,25 +12,22 @@ import scipy
 import argparse
 ############### Argparser #############
 
-
 def make_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data", help="Give the path to the dataset to be worked on.",
                         type=str)
     return parser
 
-
 parser = make_argparser()
 args = parser.parse_args()
-
 data_path = args.data
-
 extr = pd.read_csv(data_path)
-# connectors calculate the distance between every pair of nodes based on their 3D location
-# connectors
+
 # add correct times
 extr['time']=pd.to_datetime(extr['time'])
 
+# connectors calculate the distance between every pair of nodes based on their 3D location
+# connectors
 # distance between x coordinates of two nodes
 def grid_2d_dx(x_s, x_t):
     dx = x_t - x_s
@@ -40,7 +37,6 @@ def grid_2d_dx(x_s, x_t):
 def grid_2d_dy(y_s, y_t):
     dy = y_t - y_s
     return dy
-
 
 # selectors
 def s_grid_2d_dx(dx, sources, targets):
@@ -56,7 +52,6 @@ def s_grid_2d_dy(dy, sources, targets):
     return sources, targets
 
 
-  
 # create the graph
 g = dg.DeepGraph(extr)
 
@@ -93,16 +88,14 @@ cpv, gv = g.partition_nodes('cp', feature_funcs, return_gv=True)
 cpv['g_ids'] = gv['g_id'].apply(set)
 # append cardinality of g_id sets
 cpv['n_unique_g_ids'] = cpv['g_ids'].apply(len)
-pd.to_datetime(cpv['time_amax'])
-pd.to_datetime(cpv['time_amin'])
 # append time spans
 cpv['dt'] = cpv['time_amax'] - cpv['time_amin']
-
+#rename feature name
 cpv.rename(columns={'daily_mag_sum': 'HWMId_magnitude'}, inplace=True)
 
 # discard singular components
 cpv.drop(0, inplace=True)
-
+#optional, can be deleted
 print(cpv)
 # save cpv
 cpv.to_csv(path_or_buf = "../../Results/cpv.csv", index=False)
