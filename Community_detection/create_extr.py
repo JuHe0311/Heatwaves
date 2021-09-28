a6a8393a93125163b352792ddfe1aba6163f3235
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-09-28 10:38:49 +0200

    Create create_extr.py

---
+++ Community_detection/create_extr.py
@@ -0,0 +1,15 @@
+# data i/o
+import os
+import xarray
+
+# for plots
+import matplotlib.pyplot as plt
+
+# the usual
+import numpy as np
+import pandas as pd
+import sklearn
+import extr 
+
+### Argparser ###
+
