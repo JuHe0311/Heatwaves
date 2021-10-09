18b8ea3edc4e57460afa7e23d28f5f786323d6e0
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-10-09 12:08:53 +0200

    Create k_means.py

---
+++ Community_detection/k_means.py
@@ -0,0 +1,42 @@
+# Imports:
+import os
+import xarray
+# for plots
+import matplotlib.pyplot as plt
+# the usual
+import numpy as np
+import deepgraph as dg
+import pandas as pd
+import itertools
+import scipy
+import argparse
+import sklearn
+import plotting as pt
+import cppv as cp
+from sklearn.cluster import KMeans
+import seaborn as sns
+
+############### Argparser ################
+
+def make_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-de", "--data_extreme", help="Give the path to the extreme value dataset.",
+                        type=str)
+    parser.add_argument("-do", "--data_original", help="Give the path to the original value dataset.",
+                        type=str)
+    return parser
+
+parser = make_argparser()
+args = parser.parse_args()
+
+extr = pd.read_csv(args.data_extreme)
+vt = pd.read_csv(args.data_original)
+g,cpv = cp.create_cpv(extr,vt)
+
+km = KMeans(n_clusters=5)
+y_predicted = km.fit_predict(cpv[['longitude_mean','latitude_mean']])
+cpv['F'] = y_predicted
+
+sns_plot = sns.scatterplot(cpv['longitude_mean'],cpv['latitude_mean'], hue=cpv['F'])
+sns_plot.savefig("../../Results/k_means.png")
+
