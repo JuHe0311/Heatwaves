de6d225ae4f1abd68b026bbf0d6bface1bc2bf02
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-10-27 08:44:54 +0200

    Create filter_small.py

---
+++ Community_detection/filter_small.py
@@ -0,0 +1,38 @@
+# Imports:
+import os
+import xarray
+# for plots
+import matplotlib.pyplot as plt
+# the usual
+import numpy as np
+import deepgraph as dg
+import pandas as pd
+import itertools
+import scipy
+import argparse
+import sklearn
+from sklearn.model_selection import ShuffleSplit
+import seaborn as sns
+from scipy.cluster.hierarchy import linkage, fcluster
+import cppv
+import con_sep as cs
+import plotting as pt
+
+############### Argparser #################
+
+def make_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--data", help="Give the path to the dataset to be worked on.",
+                        type=str)
+    parser.add_argument("-do", "--original_data", help="Give the path to the original dataset to be worked on.",
+                        type=str)
+    return parser
+
+parser = make_argparser()
+args = parser.parse_args()
+extr = pd.read_csv(args.data)
+vt = pd.read_csv(args.original_data)
+
+g,cpg,cpv = cppv.create_cpv(extr,vt)
+
+print(head(cpv))
