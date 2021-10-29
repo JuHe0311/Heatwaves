654cfce228368b09a1ad4e7d5a5dd680f4232562
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-10-29 15:42:05 +0200

    Update filter_small.py

--- Community_detection/filter_small.py
+++ Community_detection/filter_small.py
@@ -24,14 +24,12 @@ def make_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data", help="Give the path to the dataset to be worked on.",
                         type=str)
-    parser.add_argument("-do", "--original_data", help="Give the path to the original dataset to be worked on.",
-                        type=str)
+
     return parser
 
 parser = make_argparser()
 args = parser.parse_args()
 extr = pd.read_csv(args.data)
-vt = pd.read_csv(args.original_data)
 # sort by time
 extr.sort_values('time', inplace=True)
 
