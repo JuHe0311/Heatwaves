2d91ecbea9239e01e668a0f6cf3c14903a464c4b
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-11-02 15:00:38 +0100

    Update tmp.py

--- Community_detection/tmp.py
+++ Community_detection/tmp.py
@@ -1,4 +1,4 @@
 import os
 
-cmd = "ffmpeg -y -r 5 -i ../../Results/tmp/pcp_%03d.png -c:v libx264 -r 20 -vf scale=2052:1004 {}.mp4"
+cmd = "ffmpeg -y -r 1 -i ../../Results/tmp/pcp_%03d.png -c:v libx264 -r 20 -vf scale=2052:1004 {}.mp4"
 os.system(cmd.format('../../Results/tmp/pcp'))
