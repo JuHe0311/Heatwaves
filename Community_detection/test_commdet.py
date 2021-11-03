dc9b1be5a73a7e045dc2efaf101da13e3c8b3054
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-11-03 10:14:11 +0100

    Update test_commdet.py

--- Community_detection/test_commdet.py
+++ Community_detection/test_commdet.py
@@ -50,7 +50,7 @@ args = parser.parse_args()
 
 gv = pd.read_csv(args.data_extreme)
 
-cp.cr_cpv(gv)
+cpv=cp.cr_cpv(gv)
 # calculate threshold
 # calculate 95th percentile as threshold from the Intersection strength between the supernodes
 cpg = dg.DeepGraph(cpv)
