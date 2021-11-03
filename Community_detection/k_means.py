d97f144c422a5f57f18cf8d5d2cefea19bbd4bb7
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-11-03 11:19:01 +0100

    Update k_means.py

--- Community_detection/k_means.py
+++ Community_detection/k_means.py
@@ -28,7 +28,7 @@ parser = make_argparser()
 args = parser.parse_args()
 
 gv = pd.read_csv(args.data)
-g,cpv = cp.cr_cpv(extr,vt)
+g,cpv = cp.cr_cpv(gv)
 cpv['cos_lat'] = cpv['latitude_mean']*cos(cpv['latitude_mean'])
 km = KMeans(n_clusters=5)
 y_predicted = km.fit_predict(cpv[['longitude_mean','latitude_mean','cos_lat']])
