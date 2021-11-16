008df2a126b506a270ce87d0e4b1b27d8c7bda96
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-11-16 09:23:02 +0100

    Update plotting.py

--- Community_detection/plotting.py
+++ Community_detection/plotting.py
@@ -124,6 +124,41 @@ def plot_families(number_families,fgv,v,plot_title):
     
     obj['fig'].savefig('../../Results/%s_fam_%s.png' % (plot_title,F),
                        dpi=300, bbox_inches='tight')
+
+def plot_families5000(number_families,fgv,v,plot_title):
+  families = np.arange(number_families)
+  for F in families:
+
+    # for easy filtering, we create a new DeepGraph instance for 
+    # each component
+    gt = dg.DeepGraph(fgv.loc[F])
+
+    # configure map projection
+    kwds_basemap = {'llcrnrlon': v.longitude.min() - 1,
+                    'urcrnrlon': v.longitude.max() + 1,
+                    'llcrnrlat': v.latitude.min() - 1,
+                    'urcrnrlat': v.latitude.max() + 1}
+
+    # configure scatter plots
+    kwds_scatter = {'s': 1,
+                        'c': gt.v.n_cp_nodes.values,
+                        'cmap': 'viridis_r',
+                        'edgecolors': 'none'}
+
+    # create scatter plot on map
+    obj = gt.plot_map(lat='latitude', lon='longitude',kwds_basemap=kwds_basemap, kwds_scatter=kwds_scatter)
+
+    # configure plots
+    obj['m'].drawcoastlines(linewidth=.8)
+    obj['m'].drawparallels(range(-50, 50, 20), linewidth=.2)
+    obj['m'].drawmeridians(range(0, 360, 20), linewidth=.2)
+    cb = obj['fig'].colorbar(obj['pc'], fraction=.022, pad=.02)
+    cb.set_label('n_cps', fontsize=15) 
+    obj['ax'].set_title('Family {}'.format(F))
+    
+    obj['fig'].savefig('../../Results/%s_fam5000_%s.png' % (plot_title,F),
+                       dpi=300, bbox_inches='tight')
+    
     
 def raster_plot_families(cpg,plot_title):
   cpgt = dg.DeepGraph(cpg.v[cpg.v.F <= 10])
