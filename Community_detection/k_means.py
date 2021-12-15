8ef6caa64c738696edcfa91bebff22987baeccca
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-12-15 14:15:29 +0100

    Update k_means.py

--- Community_detection/k_means.py
+++ Community_detection/k_means.py
@@ -199,8 +199,8 @@ for n_clusters in range_n_clusters:
         ax2.scatter(c[0], c[1], marker="$%d$" % i, alpha=1, s=50, edgecolor="k")
 
     ax2.set_title("The visualization of the clustered data.")
-    ax2.set_xlabel("Feature space for the 1st feature")
-    ax2.set_ylabel("Feature space for the 2nd feature")
+    ax2.set_xlabel("day of year mean")
+    ax2.set_ylabel("Length of Heatwave")
 
     plt.suptitle(
         "Silhouette analysis for KMeans clustering on sample data with n_clusters = %d"
