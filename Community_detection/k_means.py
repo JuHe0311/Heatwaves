e14bd486c98242b494f6887b7cc2d3e888b8bafd
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2021-12-15 14:43:05 +0100

    Update k_means.py

--- Community_detection/k_means.py
+++ Community_detection/k_means.py
@@ -16,6 +16,7 @@ import con_sep as cp
 from sklearn.cluster import KMeans
 import seaborn as sns
 import math
+import cppv
 
 ############### Argparser ################
 
@@ -34,54 +35,7 @@ extr['time']=pd.to_datetime(extr['time'])
 extr['ytime'] = extr.time.apply(lambda x: x.dayofyear)
 # sort by time
 extr.sort_values('time', inplace=True)
-g = dg.DeepGraph(extr)
-# create the edges of the graph --> based on neighboring grids in a 3D dataset
-g.create_edges_ft(ft_feature=('itime', 1), 
-                  connectors=[cp.grid_2d_dx, cp.grid_2d_dy], 
-                  selectors=[cp.s_grid_2d_dx, cp.s_grid_2d_dy],
-                  r_dtype_dic={'ft_r': np.bool,
-                               'dx': np.int8,
-                               'dy': np.int8}, 
-                  max_pairs=1e7)
-
-# rename fast track relation
-g.e.rename(columns={'ft_r': 'dt'}, inplace=True)
-# all singular components (components comprised of one node only)
-# are consolidated under the label 0
-g.append_cp(consolidate_singles=True)
-# we don't need the edges any more
-del g.e
-# create supernode table of connected nodes --> partitioning of the graph by the component membership of the nodes
-# feature functions, will be applied to each component of g
-feature_funcs = {'time': [np.min, np.max],
-                 'itime': [np.min, np.max],
-                 't2m': [np.mean],
-                 'magnitude': [np.sum],
-                 'latitude': [np.mean],
-                 'longitude': [np.mean], 't2m': [np.max], 'ytime':[np.mean]}
-
-# partition the node table
-cpv, gv = g.partition_nodes('cp', feature_funcs, return_gv=True)
-
-# append geographical id sets
-cpv['g_ids'] = gv['g_id'].apply(set)
-
-# append geographical id sets
-cpv['ytimes'] = gv['ytime'].apply(set)
-
-# append cardinality of g_id sets
-cpv['n_unique_g_ids'] = cpv['g_ids'].apply(len)
-
-# append time spans
-cpv['dt'] = cpv['time_amax'] - cpv['time_amin']
-
-# append time spans
-cpv['timespan'] = cpv.dt.dt.days+1
-
-cpv.rename(columns={'magnitude_sum': 'HWMId_magnitude'}, inplace=True)
-# discard singular components
-cpv.drop(0, inplace=True)
-
+g,gv,cpv = cppv.create_cpv(extr)
 
 fig, ax = plt.subplots(figsize=(9,6), nrows=1, ncols=1)
 levels=10
