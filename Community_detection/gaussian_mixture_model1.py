7cfb06970b63708c354f1dded0cc9561ade01aa5
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-02-11 11:42:37 +0100

    Update gaussian_mixture_model1.py

--- Community_detection/gaussian_mixture_model1.py
+++ Community_detection/gaussian_mixture_model1.py
@@ -38,16 +38,17 @@ cpv = pd.read_csv(args.data)
 X = pd.DataFrame(columns = [ 'ytime_mean', 'timespan'])
 X['ytime_mean'] = cpv.ytime_mean
 X['timespan'] = cpv.timespan
-# define the model
-model = GaussianMixture(n_components=3)
-# fit the model
-model.fit(X)
-# assign a cluster to each example
-yhat = model.predict(X)
-X['cluster'] = yhat
-# retrieve unique clusters
-clusters = unique(yhat)
-# create scatter plot for samples from each cluster
-fig = sns.lmplot(x='ytime_mean', y='timespan', data=X, hue='cluster', fit_reg=False)
-# show the plot
-fig.savefig("../../Results/gaussian1.png")
+for n in range(2,3,4,5,6):
+    # define the model
+    model = GaussianMixture(n_components=n)
+    # fit the model
+    model.fit(X)
+    # assign a cluster to each example
+    yhat = model.predict(X)
+    X['cluster'] = yhat
+    # retrieve unique clusters
+    clusters = unique(yhat)
+    # create scatter plot for samples from each cluster
+    fig = sns.lmplot(x='ytime_mean', y='timespan', data=X, hue='cluster', fit_reg=False)
+    # show the plot
+    fig.savefig("../../Results/gaussian1_%s.png" % n)
