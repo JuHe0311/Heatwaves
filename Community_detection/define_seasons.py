f630d108ad25efacf842ebf5ffda41dea7db0f61
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-08 08:46:43 +0200

    Create define_seasons.py

---
+++ Community_detection/define_seasons.py
@@ -0,0 +1,27 @@
+# Imports:
+import numpy as np
+import deepgraph as dg
+import pandas as pd
+import argparse
+
+############### Argparser #################
+
+def make_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', "--data", help="Give the path to the cluster gv dataset.",
+                       type=str)
+
+    return parser
+
+parser = make_argparser()
+args = parser.parse_args()
+gv = pd.read_csv(args.data)
+
+gv['time']=pd.to_datetime(gv['time'])
+gv['month'] = gv.time_x.dt.month
+gv['year'] = gv.time_x.dt.year
+
+season = [gv.month.quantile(q=0.1),gv.month.quantile(q=0.9)]
+
+print(season)
+
