d2bccd11e3f95b7f9a842271c4e5831fbc6ce137
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-16 08:29:57 +0200

    Update plot_correlation_time.py

--- Community_detection/plot_correlation_time.py
+++ Community_detection/plot_correlation_time.py
@@ -55,12 +55,12 @@ hwmid_corr.drop(hwmid_corr.loc[hwmid_corr['significant']==0].index,inplace=True)
 
 for val in hwmid_clust:
     hwmid_filt = hwmid_corr[hwmid_corr.cluster == val]
-    print(mk.original_test(hwmid_filt))
+    print(mk.original_test(hwmid_filt.corr))
     plot.scatter(hwmid_filt,'hwmid_%s' % val)
 
 for val in n_nodes_clust:
     n_nodes_filt = n_nodes_corr[n_nodes_corr.cluster == val]
-    print(mk.original_test(n_nodes_filt))
+    print(mk.original_test(n_nodes_filt.corr))
     plot.scatter(n_nodes_filt, 'n_nodes_%s' % val)
 
   
