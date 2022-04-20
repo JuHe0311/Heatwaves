2ca34091c6b5fb9571535676b8a764b5f9caec34
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-20 12:25:23 +0200

    Update NDVI_correlation.py

--- Community_detection/NDVI_correlation.py
+++ Community_detection/NDVI_correlation.py
@@ -43,7 +43,7 @@ args = parser.parse_args()
 ndvi = pd.read_csv(args.ndvi_data)
 t = pd.read_csv(args.temperature_data)
 season = args.season
-t['time_x']=pd.to_datetime(t['time_x'])
+t['time']=pd.to_datetime(t['time'])
 t['year'] = t.time_x.dt.year
 
 # perform the correlation
