f4cadbfeff9bcc5c97d9e575cfe1f8af20be426f
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-22 11:07:13 +0200

    Update new2.py

--- Community_detection/new2.py
+++ Community_detection/new2.py
@@ -30,6 +30,22 @@ def calc_mag(data):
         mag = 0
     return mag
 
+# calculate the 95th percentile of a list
+def calc_percentile(a_list):
+    threshold = np.percentile(a_list,95)
+    return threshold
+
+# creates the list the 95th percentile should be calculated on and then calls calc_percentile
+def calc_perc(lst):
+    a_list = []
+    for l in lst:
+        for i in range(len(l)):
+            a_list.append(l[i])
+    return calc_percentile(a_list)     
+ 
+# convert temperature from kelvin to degrees celcius
+def conv_to_degreescelcius(data):
+    data.t2m = data.t2m - 273.15  
 
 ### Argparser ###
 
