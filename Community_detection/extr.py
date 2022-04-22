b77bc9a448af209e2dad9a6ee96ee8e8709a7586
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-22 11:04:58 +0200

    Update extr.py

--- Community_detection/extr.py
+++ Community_detection/extr.py
@@ -1,41 +1,19 @@
-# data i/o
-import os
-import xarray
-
-# for plots
-import matplotlib.pyplot as plt
-
-# the usual
+# Imports
 import numpy as np
 import pandas as pd
 import sklearn
 #### Methods ####
 
-
 def calc_percentile(a_list):
     threshold = np.percentile(a_list,95)
     return threshold
-
-def calc_percentile0(a_list):
-    threshold = np.percentile(a_list,90)
-    return threshold
-
 
 def calc_perc(lst):
     a_list = []
     for l in lst:
         for i in range(len(l)):
             a_list.append(l[i])
-    return calc_percentile(a_list) 
-
-
-def calc_perc0(lst):
-    a_list = []
-    for l in lst:
-        for i in range(len(l)):
-            a_list.append(l[i])
-    return calc_percentile0(a_list) 
-     
+    return calc_percentile(a_list)     
  
 # function to convert temperature from kelvin to degrees celcius
 def conv_to_degreescelcius(data):
