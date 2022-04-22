b5e0f72fcac3bc03e8145be081601ef75f47dbae
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-22 11:03:35 +0200

    Update con_sep.py

--- Community_detection/con_sep.py
+++ Community_detection/con_sep.py
@@ -1,20 +1,19 @@
+# This module contains all connectors and selectors needed for deep graphs 
+
+# Imports
 import numpy as np
 
-#### this module contains all connectors and selectors needed for deep graphs #####
-
-# connectors calculate the distance between every pair of nodes based on their 3D location
-# connectors
+# connectors and selectors for Supernode (heat wave) generation
 # distance between x coordinates of two nodes
 def grid_2d_dx(x_s, x_t):
     dx = x_t - x_s
     return dx
-  
+
 # distance between y coordinates of two nodes
 def grid_2d_dy(y_s, y_t):
     dy = y_t - y_s
     return dy
 
-# selectors
 def s_grid_2d_dx(dx, sources, targets):
     dxa = np.abs(dx)
     sources = sources[dxa <= 1]
@@ -27,20 +26,7 @@ def s_grid_2d_dy(dy, sources, targets):
     targets = targets[dya <= 1]
     return sources, targets
 
-
-# create edges between nodes in one family and add to the matrix count_edges
-# connector
-def same_fams(F_s, F_t):
-    dist = F_s - F_t
-    return dist
-
-# selector
-def sel_fams(dist,sources, targets):
-    sources = sources[dist == 0]
-    targets = targets[dist == 0]
-    return sources, targets  
-
-# create superedges between the supernodes to find heatwave clusters with strong regional overlaps
+# create superedges between the supernodes to find heatwave clusters with strong spatial overlaps
 # compute intersection of geographical locations
 def cp_node_intersection(g_ids_s, g_ids_t):
     intsec = np.zeros(len(g_ids_s), dtype=object)
@@ -62,4 +48,4 @@ def time_dist(dtime_amin_s, dtime_amin_t):
     dt = dtime_amin_t - dtime_amin_s
     return dt
 
-  
+ 
