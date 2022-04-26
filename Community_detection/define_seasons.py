372889d56276aca5596225fcbf7a5e6881ed0fc4
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-04-26 16:20:53 +0200

    Update define_seasons.py

--- Community_detection/define_seasons.py
+++ Community_detection/define_seasons.py
@@ -17,6 +17,7 @@ gv = pd.read_csv(args.data)
 gv['time']=pd.to_datetime(gv['time'])
 gv['month'] = gv.time.dt.month
 
+# calculate seasons of the heat wave family as the 10th (start of season) and 90th qantile (end of season) of the month distribution of the nodes
 season = [gv.month.quantile(q=0.1),gv.month.quantile(q=0.9)]
 
 print(season)
