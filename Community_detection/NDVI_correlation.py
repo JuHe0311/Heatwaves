bc5612a269c79ec5e75c0727a608233abe64b727
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-05-05 09:36:03 +0200

    Update NDVI_correlation.py

--- Community_detection/NDVI_correlation.py
+++ Community_detection/NDVI_correlation.py
@@ -67,12 +67,13 @@ for clust in upgma_clust:
         # corr_matrix: g_id - ndvi - n_nodes - hwmid_sum
         corr_matrix = seasonal_measures(g.v,season,ndvig.v)
         print(corr_matrix)
-        corr1,p_value1 = correlate(corr_matrix.n_nodes,corr_matrix.ndvi)
-        corr2,p_value2 = correlate(corr_matrix.magnitude_sum,corr_matrix.ndvi)
-        df1 = {'year': y, 'cluster': clust, 'corr': corr1,'p_value':p_value1}
-        n_nodes_corr = n_nodes_corr.append(df1, ignore_index = True)
-        df2 = {'year': y, 'cluster': clust, 'corr': corr2,'p_value':p_value2}
-        hwmid_corr = hwmid_corr.append(df2, ignore_index = True)
+        if (corr_matrix.ndvi.isnull().all()!):
+            corr1,p_value1 = correlate(corr_matrix.n_nodes,corr_matrix.ndvi)
+            corr2,p_value2 = correlate(corr_matrix.magnitude_sum,corr_matrix.ndvi)
+            df1 = {'year': y, 'cluster': clust, 'corr': corr1,'p_value':p_value1}
+            n_nodes_corr = n_nodes_corr.append(df1, ignore_index = True)
+            df2 = {'year': y, 'cluster': clust, 'corr': corr2,'p_value':p_value2}
+            hwmid_corr = hwmid_corr.append(df2, ignore_index = True)
 
 
 n_nodes_corr.to_csv(path_or_buf = "../../Results/n_nodes_correlation.csv", index=False)
