0b7c5d148ba8c5007fc1c72608349134772e6864
JuHe0311 <61434357+JuHe0311@users.noreply.github.com>
2022-05-21 09:41:03 +0200

    Update individual_correlation.py

--- Community_detection/individual_correlation.py
+++ Community_detection/individual_correlation.py
@@ -90,7 +90,7 @@ def plot_hits(fgv,v,plot_title):
     cb.set_label('Number of Heatwave Days', fontsize=15) 
     obj['ax'].set_title('%s' % (plot_title))
     
-    obj['fig'].savefig('../../Results/fam1/Heatwavedays_indiv_corr_%s.png' % plot_title,
+    obj['fig'].savefig('../../Results/fam2/Heatwavedays_indiv_corr_%s.png' % plot_title,
                        dpi=300, bbox_inches='tight')
 
     
@@ -123,7 +123,7 @@ def plot_families(fgv,v,plot_title):
     cb.set_label('Number of Heatwaves', fontsize=15) 
     obj['ax'].set_title('%s' % (plot_title))
     
-    obj['fig'].savefig('../../Results/fam1/indiv_corr_%s.png' % plot_title,
+    obj['fig'].savefig('../../Results/fam2/indiv_corr_%s.png' % plot_title,
                        dpi=300, bbox_inches='tight')
 
 
